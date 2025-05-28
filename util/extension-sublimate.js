4cd934c12d20c76e3107cfa60c317d0f12e0ea83
redman13 <godslayerakp@gmail.com>
2025-05-28 13:48:02 -0700

    should fix that function

--- util/extension-sublimate.js
+++ util/extension-sublimate.js
@@ -1,8 +1,20 @@
 const manager = Scratch.vm.extensionManager;
+/**
+ * Aims to protect me from violating the rules about distributing game content
+ */
 export function fixForServ00Rules() {
     for (const id in manager.workerURLs) {
         const url = manager.workerURLs[id];
-        if (!/^https?:\/\/godslayerakp\.serv00\.net/.test(url))
-            manager.workerURLs[id] = `data:application/javascript,${encodeURIComponent(manager.extUrlCodes[url])}`;
+        if (/^https?:\/\/godslayerakp\.serv00\.net/.test(url)) {
+            const code = manager.extUrlCodes[url];
+            const hash = manager.extensionHashes[url];
+            if (!code) continue;
+            const newUrl = `data:application/javascript,${encodeURIComponent(code)}`;
+            manager.workerURLs[id] = url;
+            delete manager.extUrlCodes[url];
+            manager.extUrlCodes[newUrl] = code;
+            delete manager.extensionHashes[url];
+            manager.extensionHashes[newUrl] = hash;
+        }
     }
 }
